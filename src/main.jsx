2da57681e5a6dd25a2af6bddc57e64409ee0ba3e
Wladimick <61322487+wladimick@users.noreply.github.com>
2026-08-03 18:50:02 -0400

    feat: agregar rutas pública y administrativa de Nicol

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import NicolShare from './components/NicolShare'
 import './index.css'
+
+const params = new URLSearchParams(window.location.search)
+const nicolToken = params.get('nicol')
+const isNicolAdmin = params.get('nicol-admin') === '1'
+
+let content = <App />
+if (isNicolAdmin) content = <NicolShare mode="admin" />
+else if (nicolToken) content = <NicolShare mode="public" token={nicolToken} />
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {content}
   </React.StrictMode>,
 )
