e991cf25455176965a750b19ac367e664fd0a076
Wladimick <61322487+wladimick@users.noreply.github.com>
2026-08-23 15:57:52 -0400

    feat: expose secure Mercado Pago credential route

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import NicolCardAdmin from './components/NicolCardAdmin'
 import NicolPublicCyclesVisual from './components/NicolPublicCyclesVisual'
 import NicolRecurringAdmin from './components/NicolRecurringAdmin'
 import MercadoPagoAdmin from './components/MercadoPagoAdmin'
+import MercadoPagoCredentials from './components/MercadoPagoCredentials'
 import AppStatusOverlay from './components/AppStatusOverlay'
 import './index.css'
 
@@ -12,9 +13,11 @@ const params = new URLSearchParams(window.location.search)
 const nicolToken = params.get('nicol')
 const nicolAdminMode = params.get('nicol-admin')
 const mercadoPagoAdminMode = params.get('mercadopago-admin')
+const mercadoPagoCredentialsMode = params.get('mercadopago-credentials')
 
 let content = <App />
-if (mercadoPagoAdminMode === '1') content = <MercadoPagoAdmin />
+if (mercadoPagoCredentialsMode === '1') content = <MercadoPagoCredentials />
+else if (mercadoPagoAdminMode === '1') content = <MercadoPagoAdmin />
 else if (nicolAdminMode === 'recurrentes') content = <NicolRecurringAdmin />
 else if (nicolAdminMode === '1') content = <NicolCardAdmin />
 else if (nicolToken) content = <NicolPublicCyclesVisual token={nicolToken} />
