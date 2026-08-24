40c80e857115828566b40702f6ad2bf081ea3de5
Wladimick <61322487+wladimick@users.noreply.github.com>
2026-08-23 21:43:52 -0400

    Use official statement forecast floors in projection

--- src/components/ProjectionAudited.jsx
+++ src/components/ProjectionAudited.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useMemo, useState } from 'react'
 import ProjectionWithBalanceStatus from './ProjectionWithBalanceStatus'
 import { fetchMercadoPagoStatus } from '../services/mercadoPagoService'
 import { fetchSalarySlips } from '../services/salaryService'
+import { fetchBillingForecasts } from '../services/billingForecastsService'
 import { monthKeyCL } from '../lib/financialDates'
+import { withStatementForecastFloors } from '../lib/installmentAudit'
 import {
   coverReservePayables,
   withMercadoPagoFreeBalance,
@@ -12,13 +14,19 @@ import {
 export default function ProjectionAudited(props) {
   const [mpStatus, setMpStatus] = useState(null)
   const [salarySlips, setSalarySlips] = useState([])
+  const [billingForecasts, setBillingForecasts] = useState([])
 
   useEffect(() => {
     let cancelled = false
-    Promise.allSettled([fetchMercadoPagoStatus(), fetchSalarySlips()]).then(([mpResult, salaryResult]) => {
+    Promise.allSettled([
+      fetchMercadoPagoStatus(),
+      fetchSalarySlips(),
+      fetchBillingForecasts(),
+    ]).then(([mpResult, salaryResult, forecastResult]) => {
       if (cancelled) return
       if (mpResult.status === 'fulfilled') setMpStatus(mpResult.value || null)
       if (salaryResult.status === 'fulfilled') setSalarySlips(salaryResult.value || [])
+      if (forecastResult.status === 'fulfilled') setBillingForecasts(forecastResult.value || [])
     })
     return () => { cancelled = true }
   }, [])
@@ -41,12 +49,18 @@ export default function ProjectionAudited(props) {
     5,
   ), [props.incomeList, salarySlips])
 
+  const installmentDebts = useMemo(() => withStatementForecastFloors(
+    props.installmentDebts || [],
+    billingForecasts,
+  ), [props.installmentDebts, billingForecasts])
+
   return (
     <ProjectionWithBalanceStatus
       {...props}
       accounts={accounts}
       payables={payables}
       incomeList={incomeList}
+      installmentDebts={installmentDebts}
     />
   )
 }
